56f9145d7bdf74f59367fb30b716be4b5c39c0ef
Waliuddin018 <waliuddin002@gmail.com>
2023-12-14 23:47:18 +0700

    FINAL WEB

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <strong>&copy; <a class="border-bottom" href="{{ route('admin') }}">Job-In Inventory</a>, All
+        Right Reserved.
+
+        Designed By <a class="border-bottom" target="_blank" href="https://github.com/Waliuddin018">Me</a>
+</footer>
---
+++ resources/views/partials/about.blade.php
@@ -0,0 +1,24 @@
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="row g-5 align-items-center">
+            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                <div class="row g-0 about-bg rounded overflow-hidden">
+                    <div class="col-6 text-start">
+                        <img class="img-fluid w-100" src="img/download (1).jpg">
+                    </div>
+                    <div class="col-6 text-start">
+                        <img class="img-fluid" src="img/download (2).jpg" style="width: 85%; margin-top: 15%;">
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+                <h1 class="mb-4">Selamat datang di Job-In</h1>
+                <p class="mb-4">Kami di Job-In percaya bahwa setiap individu berhak untuk menemukan pekerjaan yang sesuai dengan passion, keterampilan, dan ambisi mereka.</p>
+                <p><i class="fa fa-check text-primary me-3"></i>Ragam Peluang Karir</p>
+                <p><i class="fa fa-check text-primary me-3"></i>Proses Pencarian yang Mudah</p>
+                <p><i class="fa fa-check text-primary me-3"></i>Riset Pekerjaan dan Industri</p>
+                <a class="btn btn-primary py-3 px-5 mt-3" href="{{ route('about') }}">Baca Selengkapnya</a>
+            </div>
+        </div>
+    </div>
+</div>
